11aae89bd873327f2abcb86abe4bb5b4421fadfe
AmiMu123 <amirmustefa707@gmail.com>
2020-02-16 11:45:48 +0300

    ES6 let and const

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 class App extends Component {
   render() {
-    var helloWorld =
-      "welcome to the road to learn React Amir and its students  * his :)";
+    const helloWorld = "welcome to the road to learn React!";
     return (
       <div className="App">
         <h2>{helloWorld}</h2>
